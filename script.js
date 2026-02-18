6aa043a21b0b0aa356835654db8e37f2e4e4a64b
Gressilliya <lyasuh421@gmail.com>
2026-02-18 19:35:27 +0700

    Update script.js

--- script.js
+++ script.js
@@ -1,39 +1,9 @@
-// ===== DAFTAR =====
-function daftarUser(){
-
-let email = document.getElementById("emailDaftar").value;
-let password = document.getElementById("passDaftar").value;
-
-localStorage.setItem("email", email);
-localStorage.setItem("password", password);
-
-alert("Akun berhasil dibuat!");
-window.location.href = "index.html";
-
-return false;
-}
-
-
-// ===== LOGIN =====
 function loginUser(){
 
-let emailInput = document.getElementById("email").value;
-let passInput = document.getElementById("password").value;
+    alert("Login berhasil!");
 
-let email = localStorage.getItem("email");
-let password = localStorage.getItem("password");
-
-if(emailInput === email && passInput === password){
+    // pindah ke website utama
     window.location.href = "home.html";
-}else{
-    alert("Login gagal!");
-}
 
-return false;
-}
-
-
-// ===== LOGOUT =====
-function logout(){
-window.location.href="index.html";
+    return false;
 }
